8f61637081917bec9f8e514b59f862375aae5b05
Mariza <arthuralbino844@gmail.com>
2022-10-28 13:53:54 -0300

    A área do triângulo está funcionando, mas não da maneira correta

--- js/funcoes.js
+++ js/funcoes.js
@@ -6,7 +6,34 @@ function calculadora(){
     num2= document.getElementById("num2").value;
 
     //está pegando o elemento do radiobox usando esse parâmetro input [name=opt]: checked
+
     op= document.querySelector("input[name=opt]:checked").value;
     rstd= eval(parseInt(num1) + op + parseInt(num2));
     document.getElementById("mostrar-resultado").innerHTML= rstd;
 }
+
+function potenciacao(){
+    num1= document.getElementById("num1").value;
+    num2= document.getElementById("num2").value;
+
+    rstd= eval (Math.pow(parseInt(num1),parseInt(num2)));
+    document.getElementById("mostrar-resultado").innerHTML= rstd;
+}
+
+
+function triangulo(){
+    num1= document.getElementById("num1").value;
+    num2= document.getElementById("num2").value;
+
+    rstd= eval(parseInt(num1)*parseInt(num2))/2;
+    document.getElementById("mostrar-resultado").innerHTML= rstd;
+}
+
+
+/*function raiz(){
+    num1= document.getElementById("num1").value;
+    num2= document.getElementById("num2").value;
+
+    rstd= eval((parseInt(num1))+Math.sqrt(parseInt(num2)));
+    document.getElementById("mostrar-resultado").innerHTML= rstd;
+}*/
